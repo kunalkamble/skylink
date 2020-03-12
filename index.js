05a5dc94749277c047ec414c2a973b0c5a93016f
Kunal Kamble <kunal6666@gmail.com>
2020-03-12 02:05:37 -0400

    if condition for headers

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 const api = require('./src/api')
 
-const { getFolderStructure } = require('./src/getFolderStructure')
-
 const explore = async (skylink) => {
   return await api.getHeaders(skylink)
 }
--- src/api.js
+++ src/api.js
@@ -2,27 +2,21 @@
 
 const axios = require('axios')
 const { getFolderStructure } = require('./getFolderStructure')
-// async function getHeaders(skylink) {
-//   const url = `${skylink}?format=concat`
-//   try {
-//     let res = await axios.head(url);
-//     console.log(res.headers['skynet-file-metadata'].filename)
-//     return res.headers['skynet-file-metadata'].filename
-//   } catch(err) {
-//     console.log('-------------------------------------')
-//     console.log(err)
-//   }
-// }
 
 const getHeaders = async (skylink) => {
   const url = `${skylink}?format=concat`
   try {
     const resp = await axios.head(url);
-    const headers = JSON.parse(resp.headers['skynet-file-metadata'])
-    if(headers.filename) {
-      const response = getFolderStructure(headers, headers.filename)
-      return response
+    if(resp.headers['skynet-file-metadata']) {
+      const headers = JSON.parse(resp.headers['skynet-file-metadata'])
+      if(headers.filename) {
+        const response = getFolderStructure(headers, headers.filename)
+        return response
+      }
+    } else {
+      return []
     }
+    
   } catch(err) {
     console.log('-------------------------------------')
     console.log(err)
